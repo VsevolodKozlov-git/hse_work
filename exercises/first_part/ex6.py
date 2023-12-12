5a06b9e8fb3d59602ef0ed7ebb3c884116f6fd0e
VsevolodKozlov-git <vsevolod026@gmail.com>
2023-12-12 18:32:17 +0300

    added ex6

--- exercises/first_part/ex6.py
+++ exercises/first_part/ex6.py
@@ -1,3 +1,56 @@
+import pandas as pd
+from datetime import datetime
 
-def get_grade(grades, plan, excuses):
-    grades
+def main(grades, excuses, plan):
+    # prepare dataframes
+    grades = grades.T
+    plan.set_index('date', inplace=True)
+    plan = plan.to_dict()['activity']
+
+    excuses[['start_date', 'end_date']] = excuses['date'].str.split(expand=True)
+    excuses['start_date'] = pd.to_datetime(excuses['start_date'])
+    excuses['end_date'] = pd.to_datetime(excuses['end_date'])
+    excuses.drop('date', axis=1, inplace=True)
+
+    cnt_grades = {}
+    names = grades.columns
+    for name in names:
+        cnt_grades[name] = {'Seminar': {'sum': 0, 'cnt': 0},
+                            'Test': {'sum': 0, 'cnt': 0}}
+    for date, row in grades.iterrows():
+        activity = plan[date]
+        for name in names:
+            value = row[name]
+            if pd.isna(value):
+                is_valid = is_valid_excuse(excuses, date, name)
+                if is_valid:
+                    continue
+                else:
+                    cnt_grades[name][activity]['cnt'] += 1
+            else:
+                cnt_grades[name][activity]['cnt'] += 1
+                cnt_grades[name][activity]['sum'] += value
+
+    result = {}
+    for name in names:
+        avg_seminars = (cnt_grades[name]['Seminar']['sum'] /
+                        cnt_grades[name]['Seminar']['cnt'])
+        avg_tests = (cnt_grades[name]['Test']['sum'] /
+                     cnt_grades[name]['Test']['cnt'])
+        result[name] = round(0.7*avg_seminars + 0.3*avg_tests)
+    return pd.Series(result)
+
+
+def is_valid_excuse(excuses, date, name):
+    date = datetime.strptime(date, "%Y-%m-%d")
+    filtered_df = excuses[(excuses['start_date'] <= date) & (excuses['end_date'] >= date)]
+    filtered_df = filtered_df[filtered_df['name'] == name]
+    if len(filtered_df) == 0:
+        return False
+    else:
+        reason = filtered_df.iloc[0]['reason']
+        if reason == 'valid':
+            return True
+        return False
+
+# result = main(grades, excuses, plan)
---
+++ tests/test__ex6.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+import pandas as pd
+from exercises.first_part import ex6
+
+
+class TestMain(TestCase):
+    def test__main(self):
+        grades = pd.DataFrame({'2017-02-05': {'Hermione': 5, 'Ron': 2},
+                               '2017-02-12': {'Hermione': float('nan'), 'Ron': float('nan')},
+                               '2017-02-19': {'Hermione': 7.0, 'Ron': float('nan')},
+                               '2017-02-26': {'Hermione': float('nan'), 'Ron': 4.0}})
+
+        plan = pd.DataFrame({'activity': {0: 'Test', 1: 'Test', 2: 'Seminar', 3: 'Seminar'},
+                             'date': {0: '2017-02-05', 1: '2017-02-12', 2: '2017-02-19', 3: '2017-02-26'}})
+
+        excuses = pd.DataFrame({'name': {0: 'Hermione', 1: 'Hermione', 2: 'Ron', 3: 'Harry'},
+                                'date': {0: '2017-02-10 2017-02-11', 1: '2017-02-12 2017-02-12', 2: '2017-02-19 2017-02-19', 3: '2017-02-19 2017-02-19'},
+                                'reason': {0: 'valid', 1: 'valid', 2: 'invalid', 3: 'invalid'}})
+
+        actual_result = ex6.main(grades, excuses, plan)
+        expected_result = pd.Series({'Hermione': 4, 'Ron': 2})
+        for index in expected_result.index:
+            expected_value = expected_result[index]
+            actual_value = actual_result[index]
+            self.assertEqual(actual_value, expected_value)
